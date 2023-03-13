cc40335cdc4e67935e14e2a3cdb0579a68963840
ch990001 <ch990001@gmail.com>
2023-03-13 15:18:58 +0800

    開始寫程式
    將其他長度單位轉換做出來

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,6 +32,37 @@ namespace LengthCalculator
             douCM = Convert.ToDouble(txtCM.Text);
 
             txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+        }
+
+        private void txtM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douM;
+
+            douM = Convert.ToDouble(txtM.Text);
+
+            txtCM.Text = string.Format("{0:0.##########}", douM / 0.01);
+
+            txtKM.Text = string.Format("{0:0.##########}", douM / 1000);
+        }
+
+        private void txtKM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douKM;
+
+            douKM = Convert.ToDouble(txtKM.Text);
+
+            txtCM.Text = string.Format("{0:0.##########}", douKM / 0.00001);
+
+            txtM.Text = string.Format("{0:0.##########}", douKM / 0.001);
+        }
+
+        private void btnAllClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtCM.Text = "";
+            txtM.Text = "";
+            txtKM.Text = "";
         }
     }
 }
